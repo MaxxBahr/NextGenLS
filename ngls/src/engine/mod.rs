9893b86cf29a1c6927e8fad685e1cf7c678f94c5
MaxxBahr <schnee.bahr@googlemail.com>
2025-07-22 15:04:32 +0200

    added more functionality

--- ngls/src/engine/mod.rs
+++ ngls/src/engine/mod.rs
@@ -1,8 +1,11 @@
 use std::{fs, path::Path};
 use std::collections::HashSet;
+use regex::Regex;
 
 pub fn search_function(path: &Path){
-    let contents = fs::read_to_string(path).expect("Could not read file");
+    let contents = collect_files(path);
+    //iterate over hashset
+    //
 
 }
 
@@ -13,8 +16,14 @@ fn collect_files(path: &Path) -> HashSet<String>{
 }
 
 fn find_files(path: &Path, store: &mut HashSet<String>){
+    let path_to_string = path.to_str().unwrap();
     //if path is a finite path to file
     //add file to hashset
+    match path_to_string.ends_with('.')
     //else
     //recall function until file is found
+}
+
+fn file_ending(path: String) -> bool{
+
 }
