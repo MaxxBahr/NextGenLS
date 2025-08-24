e69352f2c4f98947d78871f2cae673f490033063
MaxxBahr <schnee.bahr@googlemail.com>
2025-08-24 21:08:27 +0200

    added function to recursively search files

--- ngls/src/engine/mod.rs
+++ ngls/src/engine/mod.rs
@@ -1,6 +1,8 @@
+use std::ffi::CString;
 use std::{fs, path::Path};
 use std::collections::HashSet;
 use regex::Regex;
+use libc::{opendir, readdir, dirent, closedir};
 
 pub trait file_ending {
     fn file_ending(&self) -> bool;
@@ -38,7 +40,22 @@ fn find_files(path: String, store: &mut HashSet<String>){
         //recall function until file is found
         //https://github.com/rust-lang/libc/blob/main/README.md
         // https://docs.rs/libc/latest/libc/fn.opendir.html
-        let dir_contents: Vec<String> = Vec::new();
-        find_files(path, store);
+        unsafe { 
+            let c_path = std::ffi::CString::new(path).unwrap();
+            let entries = opendir(c_path.as_ptr() as *const i8);
+            if !entries.is_null() {
+                loop {
+                    let next_entry = readdir(entries);
+                    if next_entry.is_null(){
+                        break;
+                    }
+                    let dir_entry: &dirent = &*next_entry;
+                    let c_str: &std::ffi::CStr = std::ffi::CStr::from_ptr(dir_entry.d_name.as_ptr());
+                    let filename = c_str.to_string_lossy().into_owned();
+                    find_files(filename, store);
+                }
+                closedir(entries);
+            }
+         };
     }
 }
